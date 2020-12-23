4583d26b1d23c7d543df167367cfb7d33e6167dc
Jake <72174196+acairg@users.noreply.github.com>
2020-12-23 11:30:32 -0600

    rm merging module post pdf pull switch
    removed merging code after finding that wkhtmltopdf uploads pdf's with styling included. Automator application will be included to merge due to anchors not allowing appended merge via pyPDF2

--- PDFer.py
+++ PDFer.py
@@ -1,43 +1,33 @@
 '''
   Jake Pattinson
-  2020-12-19
+  PDFer
 '''
-from xhtml2pdf import pisa 
+from xhtml2pdf import pisa
 import pdfkit
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import re
 import os
-from PyPDF2 import PdfFileMerger
+from PyPDF2 import PdfFileMerger, PdfFileWriter
 # imports ^
-
 ls = os.path.dirname(
 os.path.realpath(__file__))
-#clean slate! delete all PDF before generating 
+#clean slate! delete all PDF before generating
 filelist = [ f for f in os.listdir(ls) if f.endswith(".pdf") ]
+
+#
 for f in filelist:
     os.remove(os.path.join(ls, f))
 print(" -----------------------------------------------------------")
-print(ls) #prints directory incase you need to cd
+print(ls) #prints directory incase you need it
 print(" -----------------------------------------------------------")
-
-guideURL = input("enter the URL you wish to convert.")
-parser = 'html.parser' # or 'lxml' (preferred) or 'html5lib', if installed 
-resp = urlopen(guideURL) 
-soup = BeautifulSoup(resp, parser, from_encoding=resp.info().get_param('charset'))  
-for link in soup.find_all('a', href=True): 
-  print(link['href']) 
-  os.system("xhtml2pdf " + link['href'])
-os.system('wkhtmltopdf --load-error-handling ignore http:/#ac-gn-menustat *.pdf')
-print(" -----------------------------------------------------------")
-input('    Please, press enter to continue........... ')
-print(" -----------------------------------------------------------")
-merger = PdfFileMerger()
-for i in os.listdir(ls):
-    if i.endswith('.pdf'):
-        merger.append(i, 'r+b')
-merger.write(ls + 'MergeGuide.pdf')
-merger.close()
-for f in filelist:
-  os.remove(os.path.join(ls, f))
-print("It's Ready!")
+guideURL = input("enter the URL you wish to convert: ")
+parser = 'html.parser' # or 'lxml' (preferred) or 'html5lib', if installed
+resp = urlopen(guideURL)
+soup = BeautifulSoup(resp, parser, from_encoding=resp.info().get_param('charset'))
+x= 0
+for link in soup.find_all('a', href=True):
+  print(link['href'])
+  x=x+1
+  print(x)
+  os.system("wkhtmltopdf --lowquality --disable-smart-shrinking --load-error-handling ignore " + link['href'] + " out" + str(x) + ".pdf")
